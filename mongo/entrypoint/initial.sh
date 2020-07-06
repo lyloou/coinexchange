b1dd23595e82c19878c41199abc5cab0c4fd6297
lyloou <lyloou@qq.com>
2020-07-06 17:38:54 +0800

    update: mongo

--- mongo/entrypoint/initial.sh
+++ mongo/entrypoint/initial.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-echo "Init mongo directory."
-mkdir -p /data/mongo/
-mkdir -p /data/mongo/db
-touch /data/mongo/mongod.log
-
 echo "Creating mongo users..."
 mongo admin --host localhost -u admin -p 123456 --eval "db.createUser({user: 'admin', pwd: '123456', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
 mongo admin -u admin -p 123456 << EOF
