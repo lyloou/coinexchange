c220fec64ed2576040edc49762bfeb6cc18a16e6
lyloou <lyloou@qq.com>
2020-07-06 17:13:57 +0800

    add mongo

--- mongo-entrypoint/initial.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Creating mongo users..."
-mongo admin --host localhost -u root -p root --eval "db.createUser({user: 'admin', pwd: '123456', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
-mongo admin -u root -p root << EOF
-use hi
-db.createUser({user: 'test', pwd: 'root', roles:[{role:'readWrite',db:'hi'}]})
-EOF
-echo "Mongo users created."
---
+++ mongo/entrypoint/initial.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "Init mongo directory."
+mkdir -p /data/mongo/
+mkdir -p /data/mongo/db
+touch /data/mongo/mongod.log
+
+echo "Creating mongo users..."
+mongo admin --host localhost -u root -p 123456 --eval "db.createUser({user: 'admin', pwd: '123456', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
+mongo admin -u root -p 123456 << EOF
+use hi
+db.createUser({user: 'test', pwd: 'root', roles:[{role:'readWrite',db:'hi'}]})
+EOF
+echo "Mongo users created."
