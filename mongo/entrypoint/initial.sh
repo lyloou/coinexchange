6ea709c4899080b0235d1b831777e6a3e77e9a4d
lyloou <lyloou@qq.com>
2020-12-29 14:03:31 +0800

    add: feature redis

--- mongo/entrypoint/initial.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Creating mongo users..."
-mongo admin --host localhost -u admin -p 123456 --eval "db.createUser({user: 'admin', pwd: '123456', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
-mongo admin -u admin -p 123456 << EOF
-use bitrade
-db.createUser({user: 'root', pwd: 'root', roles:[{role:'readWrite',db:'hi'}]})
-EOF
-echo "Mongo users created."
